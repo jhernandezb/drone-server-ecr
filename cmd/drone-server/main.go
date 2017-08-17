a44c36b275c49a8dbc7ffa4415e1082e4149fa56
jhernandezb <contact@jhernandez.me>
2017-08-16 22:26:16 -0600

    drone server

---
+++ cmd/drone-server/main.go
@@ -0,0 +1,27 @@
+// Copy of https://github.com/drone/drone/blob/master/cmd/drone-server/main.go
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/drone/drone/version"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "drone-server"
+	app.Version = version.Version.String()
+	app.Usage = "drone server"
+	app.Action = server
+	app.Flags = flags
+	app.Before = before
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
